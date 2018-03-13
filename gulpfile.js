3db808ee6caec0fc4dd1cf22eb687c0ba5429c4f
lucasbaiao <lucasbaiao@live.com.pt>
2018-03-13 13:11:47 -0300

    initial

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+var gulp = require("gulp");
+var concat = require("gulp-concat");
+// import gulp from 'gulp';
+
+gulp.task('js', function(){
+    return gulp.src([
+        './node_modules/bootstrap/dist/js/bootstrap.min.js',
+        './node_modules/jquery/dist/jquery.min.js'
+    ])
+    .pipe(gulp.dest('wwwroot/js/'));
+});
+
+gulp.task('css', function(){
+    return gulp.src([
+        './styles/site.css',
+        './node_modules/bootstrap/dist/css/bootstrap.css'
+    ])
+    .pipe(concat('site.min.css'))
+    .pipe(gulp.dest('wwwroot/css'))
+});
